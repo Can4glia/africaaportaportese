6d89b2d53d15b0643fd4b2d3144b432a32c5c949
Can4glia <canaglia@yahoo.com>
2021-04-19 23:26:09 +0200

    version 0.1

--- src/app.js
+++ src/app.js
@@ -2,6 +2,7 @@ $( document ).ready(function() {
   // console.log( "developed by Can4glia" );
 
   var checkBox = $('#ms_menu-btn');
+  checkBox.prop('checked', false);
 
   $('#ms_toggle').click(function() {
 
@@ -13,7 +14,18 @@ $( document ).ready(function() {
 
       $(this).siblings(checkBox).prop('checked', false);
     }
-
   });
 
+  // smooth scroll via anchor, a native function ES2018
+  document.querySelectorAll('a[href^="#"]').forEach(anchor => {    
+    anchor.addEventListener('click', function (e) {        
+      e.preventDefault();
+      // go (smooth)
+      document.querySelector(this.getAttribute('href')).scrollIntoView({            
+        behavior: 'smooth'        
+      });
+      // and close sidebar   
+      $(this).parents('#ms_nav').siblings(checkBox).prop('checked', false);
+    });
+  });
 });
