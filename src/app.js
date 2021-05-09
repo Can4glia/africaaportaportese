b8c90db67db6356b7da641a747484054c7a0154a
Can4glia <canaglia@yahoo.com>
2021-05-09 14:06:22 +0200

    cleanup

--- src/app.js
+++ src/app.js
@@ -41,15 +41,13 @@ $( document ).ready(function() {
   //   }
   // });
 
-  // remove fragment as much as it can go without adding an entry in browser history:
-  window.location.replace("#");
+  // // remove fragment as much as it can go without adding an entry in browser history:
+  // window.location.replace("#");
 
-  // slice off the remaining '#' in HTML5:    
-  if (typeof window.history.replaceState == 'function') {
-    history.replaceState({}, '', window.location.href.slice(0, -1));
-  }
-    
-
+  // // slice off the remaining '#' in HTML5:    
+  // if (typeof window.history.replaceState == 'function') {
+  //   history.replaceState({}, '', window.location.href.slice(0, -1));
+  // }
 
   // console.log(window.location.href);
 });
