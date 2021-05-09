cb84fec29e2032bfb396fae237e7c2fba4f3c5c7
Can4glia <canaglia@yahoo.com>
2021-05-09 12:25:30 +0200

    pretty url and favicon

--- src/app.js
+++ src/app.js
@@ -40,5 +40,16 @@ $( document ).ready(function() {
   //     });
   //   }
   // });
+
+  // remove fragment as much as it can go without adding an entry in browser history:
+  window.location.replace("#");
+
+  // slice off the remaining '#' in HTML5:    
+  if (typeof window.history.replaceState == 'function') {
+    history.replaceState({}, '', window.location.href.slice(0, -1));
+  }
     
+
+
+  // console.log(window.location.href);
 });
